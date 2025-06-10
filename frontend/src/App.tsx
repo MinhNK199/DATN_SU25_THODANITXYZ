797ff2d41d2e910cf5b214b8e3aad662633ae4c4
Phong <phongphucdz@gmail.com>
2025-06-11 00:25:51 +0700

    interface cập nhật app.js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
+import ProductEdit from "./admin/components/ProductEdit";
+import ProductAdd from "./admin/components/ProductAdd";
+import Admin from "./admin/pages/Admin";
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/admin/product/add" element={<ProductAdd />} />
+<Route path="/admin/product/edit/:id" element={<ProductEdit />} />
+<Route path="/admin/product/delete/:id" element={<Admin />} />
+<Route path="/admin" element={<Admin />} />
         <Route path="*" element={<div>404 - Not Found</div>} />
       </Routes>
     </BrowserRouter>
