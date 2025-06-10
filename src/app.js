56d148b4f49f6f26b693e1d99e39d527109cc6a4
Phong <phongphucdz@gmail.com>
2025-06-10 15:16:00 +0700

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"; // Thêm dòng này
 import router from "./routers/index.js";
 import connectDB from "./config/database.js";
-
+import orderRouter from "./routers/order.js";
 dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors({
-    origin: "http://localhost:5173", // FE chạy ở cổng 5173
-    credentials: true
-}));
-
 app.use(express.json());
 
 app.use("/api", router);
+app.use("/api/orders", orderRouter);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server Đã Được Chạy Ở Cổng ${process.env.PORT || 3000}🚀`);
