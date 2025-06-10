e8aac5f7aa97d22d6de0400fcd0b7974dfe72379
Phong <phongphucdz@gmail.com>
2025-06-10 13:44:00 +0700

    CORS chay FE

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; // Thêm dòng này
 import router from "./routers/index.js";
 import connectDB from "./config/database.js";
 
@@ -7,10 +8,15 @@ dotenv.config();
 const app = express();
 connectDB();
 
+app.use(cors({
+    origin: "http://localhost:5173", // FE chạy ở cổng 5173
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api", router);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server Đã Được Chạy Ở Cổng ${process.env.PORT || 3000}🚀`);
+    console.log(`Server Đã Được Chạy Ở Cổng ${process.env.PORT || 3000}🚀`);
 });
